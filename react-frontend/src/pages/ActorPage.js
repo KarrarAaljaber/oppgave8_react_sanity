18961cd01de1ea49b394daa51e9663de2f118a82
Karrar <51439971+KarrarAaljaber@users.noreply.github.com>
2022-03-28 06:53:10 +0200

    commit

--- react-frontend/src/pages/ActorPage.js
+++ react-frontend/src/pages/ActorPage.js
@@ -2,11 +2,13 @@ import Actor from "../components/Actor";
 import { getActor } from "../utils/services/actors";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { getActorWithMovies } from "../utils/services/movies";
 
 
 const ActorPage = () =>{
 
     const[actor, setActor] = useState([])
+    const[movies,setMovies] = useState([])
     const { slug } = useParams()
     const value = slug.replace(':', '')
 
@@ -17,20 +19,24 @@ const ActorPage = () =>{
          console.log(value)
         // const actors = await getActors()
             const act = await getActor(value)
-            console.log(act)
-            //console.log(actors)
-    
+            console.log(act[0].fullname)
+            const moviz = await getActorWithMovies(act[0].fullname);
+
             setActor(act)
+            setMovies(moviz[0].starsin)
             
     }, [])
 
-    const mapActor = actor?.map((m) => (
-        <Actor fullname={m.fullname} picture={m.picture} />
-      ));
+
+      
+    const mapMovies = movies?.map((m) => (<li key={m?.title}> {m?.title} </li>))
 
     return(
         <div className="actorsPage">
-            {mapActor}
+            <Actor fullname={actor[0]?.fullname} picture={actor[0]?.picture}  />
+
+            <h3> This actor stars in these movies {mapMovies}   </h3>
+                
         </div>
     )
 }
--- react-frontend/src/utils/services/movies.js
+++ react-frontend/src/utils/services/movies.js
@@ -8,6 +8,17 @@ const eventFields = `
 
 `
 
+export const getActorWithMovies = async (fullname) =>{
+  const data = await client.fetch(
+    ` *[_type=="actor" && fullname == $fullname]{
+        fullname,
+        picture,
+        "starsin": *[_type=='movie' && references(^._id)]{ title }
+      } `, { fullname}
+  )
+  return data
+}
+
 export const getMovies = async () => {
   const data = await client.fetch(`*[_type == "movie"]{${eventFields}}`);
   return data;
